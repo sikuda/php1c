70337b059aca21acdc782c33408384b8484817b5
Sergey Kudashkin <sikuda@yandex.ru>
2019-09-04 07:46:42 +0300

    new  фалй для тестирования
    New testing epf

--- output.php
+++ output.php
@@ -3,8 +3,8 @@ require_once( 'php1C_common.php');
 
 $REZULTAT = null;
 function SLOZHENIE($D,$YA){
-$REZULTAT=php1C\add1C(php1C\add1C("4",$D),$YA);
+$REZULTAT=php1C\sub1C(php1C\add1C("2",$D),$YA);
 }
-SLOZHENIE(1,2);
+SLOZHENIE("2",1);
 
  ?>
