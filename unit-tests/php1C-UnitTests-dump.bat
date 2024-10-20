06f9d5c6f9476884567852f574652ed998a769e6
Sergey Kudashkin <ya.sikuda@yandex.ru>
2024-10-20 12:30:33 +0300

    Назад в полную версию

---
+++ unit-tests/php1C-UnitTests-dump.bat
@@ -0,0 +1,3 @@
+rem set program1C="C:\Program Files\1cv8\8.3.22.2283\bin\1cv8"
+set program1C="C:\Program Files (x86)\1cv8\8.3.24.1368\bin\1cv8"
+%program1C% DESIGNER /DumpExternalDataProcessorOrReportToFiles "./php1C-UnitTests" "php1C-UnitTests.epf" 
---
+++ unit-tests/php1C-UnitTests-restore.bat
@@ -0,0 +1,3 @@
+rem set program1C="C:\Program Files\1cv8\8.3.22.2283\bin\1cv8"
+set program1C="C:\Program Files (x86)\1cv8\8.3.24.1368\bin\1cv8"
+%program1C% DESIGNER /LoadExternalDataProcessorOrReportFromFiles "./php1C-UnitTests/CheckRun1CPHP.xml" "php1C-UnitTests.epf" 
